b8a4fdcde5830cb2968eca2c63e32efbe1143a39
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-20 12:34:42 +0530

    Add files via upload

---
+++ if().py
@@ -0,0 +1,9 @@
+def kavitha_varsha(j):
+	if j:
+		print("Hpy baday queen",j,"\U0001F618","\U0001F382")
+		print("Always keep smiling","\U0001F917","\U0001F381")
+		print("U r special fr me" ,"\U0001F497")
+	else:
+		pass
+
+kavitha_varsha("Saritha")
