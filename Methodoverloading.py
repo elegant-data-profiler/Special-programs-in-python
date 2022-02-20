d756732a6653c2c3ca84ba0bb960468b99c92ca1
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-20 12:33:33 +0530

    Add files via upload

---
+++ Methodoverloading.py
@@ -0,0 +1,10 @@
+class raider:
+	def bike(self,b):
+		print('raiding')
+	def bike(self,a):
+		print("bike raiding")
+class diver:
+	def car(self):
+		print("car driving")
+r=raider()
+print(r.bike(1))
