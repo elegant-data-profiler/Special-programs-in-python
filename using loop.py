7d7dd314ae9ba83695fca142766a405ec0f41d7e
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-23 10:38:44 +0530

    Add files via upload

---
+++ using loop.py
@@ -0,0 +1,8 @@
+#Write a program to print the natural numbers and total sum of those numbers by using forloop
+def sample():
+    s=0
+    for i in range(10):
+        s=s+i
+    return s
+        
+print(sample())
