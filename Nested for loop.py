18c18170d7be24d4c6604226287f88a3d29f9582
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-24 16:42:08 +0530

    Add files via upload

---
+++ Nested for loop.py
@@ -0,0 +1,57 @@
+
+# write a program using nested for loop
+
+def sample():
+    L3 = []
+    for i in [10,20,30]:
+        for j in [1,2,3]:
+            L3.append(i+j)
+    print(L3)
+sample() 
+ 
+ # wirte a program using nested for loop
+
+def func():
+    L3 = []
+    for i in ["Hello"]:
+        for j in ["World"]:
+            L3.append(i + j)
+    print(L3)
+func()  
+
+# write a program using nested for loop
+
+
+def my_list():
+    List3 = []
+    for i in ["Big"]:
+        for j in ["Company"]:
+            List3.append(i+j)
+    print(List3)        
+my_list() 
+
+# write a program using nested for loop
+
+
+for i in range(3):
+	for j in range(4):
+		print("i = ",i,"\t","j = ",j)
+
+
+# write a program using nested for loop
+
+
+for i in range(1,4):
+	for j in range(1,3):
+		for k in range(1,3):
+			print("i=",i,"j=",j,"k=",k)
+			sum = i+j+k
+print("sum = ",sum)		
+
+
+
+
+
+
+
+
