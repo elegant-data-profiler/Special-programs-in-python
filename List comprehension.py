851b8d66c6817bfb71471dea74a9017e841a3d8a
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-24 16:41:38 +0530

    Add files via upload

---
+++ List comprehension.py
@@ -0,0 +1,31 @@
+
+
+# write a program using list comprehension
+
+
+def list_comprehension(x):
+    print([i for i in x])
+list_comprehension("Data Science")
+
+
+# write a program using list comprehension
+
+def list_comprehension(n):
+    print([i for i in n])
+list_comprehension("python")    
+
+
+# write a program using list comprehension
+
+def list_comprehension(a,b):
+    print([i+j for i in[a]  for j in [b]])
+list_comprehension("Hello","World")    
+
+
+
+# write a program using list comprehension
+L1 = [10,20,30]
+L2 = [1,2,3]
+print([i+j for i in L1 for j in L2])
+
+
